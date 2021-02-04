50b291689e1e6b4b8749000200555433d36a6717
Steve Gossip <stephengossip@gmail.com>
2021-02-04 17:05:45 +0000

    added some more styling to the frontend and then set up a server side with node, express with the aim to add login authentication firstly.

---
+++ client/src/components/GoalForm.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import '../style/GoalFormStyle.css';
+
+class GoalForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: '',
+      goalDescription: '',
+      timescale: '',
+      completionDate: ''
+
+    }
+
+    this.handleGoalSubmit = this.handleGoalSubmit.bind(this);
+    this.handleGoalChange = this.handleGoalChange.bind(this);
+    this.handleTimeScaleChange = this.handleTimeScaleChange.bind(this);
+  }
+
+  handleGoalChange(event) {
+    this.setState({
+      goalDescription: event.target.value
+    })
+  }
+
+  handleTimeScaleChange(event) {
+    this.setState({
+      timescale: event.target.value
+    })
+  }
+
+  handleGoalSubmit(event) {
+    event.preventDefault();
+    const startDate = (new Date(Date.now())).toDateString();
+    const goalDescription = this.state.goalDescription;
+    console.log("Goal: ", goalDescription)
+    const timescale = this.state.timescale;
+    if (!goalDescription || !timescale) {
+      return
+    }
+    this.props.handleGoalToSubmit({startDate: startDate, goalDescription: goalDescription, timescale: timescale})
+    this.setState({
+      startDate: '',
+      goalDescription: '',
+      timescale: '',
+      completionDate: ''
+    })
+
+
+  }
+  render() {
+    return(
+      <div>
+        <div className="form-title">
+          <h1>Goals</h1>
+          <p className="intro-text">Before setting you're goals <a className="non-nav-link" href="https://www.mindtools.com/pages/article/newHTE_90.htm" target="_blank" rel="noreferrer">here</a> are the five golden rules of Goal Setting  in order to set yourself up for success.</p>
+        </div>
+        <div className="goal-form">
+          <h4>Enter your Goals below to keep track of your progress: </h4>
+          <form onSubmit={this.handleGoalSubmit}>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="goal-description">What is your goal : </label>
+              </div>
+              <div className="col-75">
+                <input value={this.state.goalDescription} type="text" onChange={this.handleGoalChange} id="goal-description"></input>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-25">
+                <label htmlFor="timescale">Timescale to complete this goal : </label>
+              </div>
+              <div className="col-75">
+                <input value={this.state.timescale} type="text" onChange={this.handleTimeScaleChange} id="timescale"></input>
+              </div>
+            </div>
+            <div className="row">
+              <input className="pretty-button" type="submit" value="Set your Goal"></input>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default GoalForm;
---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Routes
+app.use('/', require('./routes/index'))
+app.use('/users', require('./routes/users'))
+
+
+
+
+
+app.listen(PORT, console.log(`Server started on port ${PORT}...`))
